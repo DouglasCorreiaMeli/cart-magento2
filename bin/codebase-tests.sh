641ae2f2e9a5f8ca12725cba07406054a11795b0
Douglas <douglas.correia@mercadolivre.com>
2021-08-26 20:47:22 -0300

    Delete codebase-tests.sh

--- bin/codebase-tests.sh
+++
@@ -1,7 +0,0 @@
-rm -rf magento2/app/code/MercadoPago;
-curl -LO https://github.com/PluginAndPartners/cart-magento2/archive/develop.zip
-unzip -qq develop.zip
-echo ":::::::::::::CHECK UNZIP FOLDER NAME::::::::::"
-ls -la
-mv cart-magento2-develop/src/* magento2/app/code
-magento2/vendor/phpunit/phpunit/phpunit --colors=always --whitelist magento2/app/code/MercadoPago/Core --coverage-text  magento2/app/code/MercadoPago/Test
