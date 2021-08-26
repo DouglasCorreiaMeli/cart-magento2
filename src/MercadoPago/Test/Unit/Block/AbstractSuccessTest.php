77908a39bf9bffaa3654481543e44484b9e84bbc
Douglas <douglas.correia@mercadolivre.com>
2021-08-26 15:44:10 -0300

    Update AbstractSuccessTest.php

--- src/MercadoPago/Test/Unit/Block/AbstractSuccessTest.php
+++ src/MercadoPago/Test/Unit/Block/AbstractSuccessTest.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace MercadoPago\Test\Unit\Block;
+
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Sales\Model\OrderFactory;
+use \MercadoPago\Core\Block\Info;
+use \PHPUnit\Framework\TestCase;
+
+class AbstractSuccessTest extends TestCase
+{
+
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+
+}
