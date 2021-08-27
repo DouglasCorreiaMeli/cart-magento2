781330a6b9b0f66ed41909fe45aaa265339ff025
Douglas <douglas.correia@mercadolivre.com>
2021-08-27 16:06:29 -0300

    Update install-plugin-develop.sh

--- bin/install-plugin-develop.sh
+++ bin/install-plugin-develop.sh
@@ -1,3 +1,3 @@
 curl -LO https://github.com/PluginAndPartners/cart-magento2/archive/develop.zip
 unzip -qq develop.zip
-mv cart-magento2-develop/src/* magento2/app/codemagento2/vendor/phpunit/phpunit/phpunit --colors=always --whitelist magento2/app/code/MercadoPago/Core --coverage-text  magento2/app/code/MercadoPago/Test
+mv cart-magento2-develop/src/* magento2/app/code
