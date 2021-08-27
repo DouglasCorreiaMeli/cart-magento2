fc4260f1043df4a750f95d4a3f86c6fbc5a453b5
Douglas <douglas.correia@mercadolivre.com>
2021-08-27 15:42:29 -0300

    Update coverage-checker.php

--- src/MercadoPago/Test/coverage-checker.php
+++ src/MercadoPago/Test/coverage-checker.php
@@ -22,15 +22,6 @@ foreach ($metrics as $metric) {
 
 $coverage = ($checkedElements / $totalElements) * 100;
 
-if ($coverage < $percentage) {
-    echo 'Code coverage is ' . round($coverage, 2) . '%, which is below the accepted ' . $percentage . '%';
-    echo "\033[01;31m -> Pull Request Rejected \033[0m";
-    // when we want to fail the pull request, just uncomment the line below
-    // exit(1);
-    //and remove else block
-}else {
-    echo 'Code coverage is ' . $coverage;
-    echo "\033[01;32m -> Pull Request OK \033[0m";
-}
-
-
+$coverageFile = fopen('coverage.txt','a');
+fwrite($coverageFile, $coverage .PHP_EOL);
+fclose($coverageFile);
